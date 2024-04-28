fcf7741c6a618a37ce9bad6c30c77e57284b1e01
waiyanhtun <waiyan.web@gmail.com>
2024-04-29 02:32:30 +0700

    adding new item

--- daytwo/js/script.js
+++ daytwo/js/script.js
@@ -1,52 +1,31 @@
-// const secondHand = document.querySelector('.second-hand');
-// const minHand = document.querySelector('.min-hand');
-// const hourHand = document.querySelector('.hour-hand');
+const hourView = document.querySelector('.hour');
+const minusView = document.querySelector('.minute');
+const secondView = document.querySelector('.second');
+const digitalView = document.querySelector('.digital-time');
 
 
-// function setDate(){
-//     const now = new Date();
-//     const seconds = now.getSeconds();
-//     const secondDeg = ((seconds / 60 ) * 360) + 90;
-//     secondHand.style.transform = `rotate(${secondDeg}deg)`;
 
-//     const mins = now.getMinutes();
-//     const minDeg = ((mins / 60 ) * 360) + ((seconds/60)*6) + 90;
-//     minHand.style.transform = `rotate(${minDeg}deg)`;
+function setDate(){
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const digitalTime = now.toLocaleTimeString('eng',{
+        hour24:true,
+        hour: "numeric",
+        minute:"numeric",
+        second:"numeric",
+    });
 
-//     const hours = now.getHours();
-//     const hourDeg = ((hours / 12 ) * 360) + ((mins/60)*30) + 90;
-//     minHand.style.transform = `rotate(${hourDeg}deg)`;
-// }
+    const secondsDeg = ((seconds / 60 ) * 360);
+    const minutesDeg = ((minutes / 60 ) * 360);
+    const hoursDeg = ((hours / 12 ) * 360);
 
-// setInterval(setDate, 1000);
+    secondView.style.transform = `rotate(${secondsDeg}deg)`;
+    minusView.style.transform = `rotate(${minutesDeg}deg)`;
+    hourView.style.transform = `rotate(${hoursDeg}deg)`;
+    digitalView.textContent = digitalTime;
 
-// setDate();
-
-const secondView = document.querySelector(".second");
-const minuteView = document.querySelector(".minute");
-const hourView = document.querySelector(".hour");
-const digitalTimeView = document.querySelector(".digital-time");
-
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const minutes = now.getMinutes();
-  const hours = now.getHours();
-  const digitalTime = now.toLocaleTimeString("en-US", {
-    hour12: true,
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-  });
-
-  const secondsDegrees = (seconds / 60) * 360;
-  const minutesDegrees = (minutes / 60) * 360;
-  const hoursDegrees = (hours / 12) * 360;
-
-  secondView.style.transform = `rotate(${secondsDegrees}deg)`;
-  minuteView.style.transform = `rotate(${minutesDegrees}deg)`;
-  hourView.style.transform = `rotate(${hoursDegrees}deg)`;
-  digitalTimeView.textContent = digitalTime;
 }
 
-setInterval(setDate, 1000);
+setInterval(setDate,1000);
